2b619b359d8021be57b404525013c53403d6cde1
Pavel Mikhaylov <pasmikh@gmail.com>
2017-07-13 17:09:52 +0300

    Delete config_default.py

--- config_default.py
+++
@@ -1,7 +0,0 @@
-days_of_week = ['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday', 'Sunday']
-operation = ''
-options = ['Info', 'Check-in/Out', 'Edit games', 'Back']
-admins = ['admin1_telegram_nickname', 'admin2_telegram_nickname']
-avail_days = []
-TOKEN = 'bot_token'
-group_id = id_of_group_chat
